48f70fddb7d90cbceda4417fb980ac7bcbe1c07c
shafiroar <shafiranrm@gmail.com>
2023-09-13 01:23:50 +0700

    Add readme.md

--- main/views.py
+++ main/views.py
@@ -6,32 +6,39 @@ def show_main(request):
         {'product': 'Whiskas Kitten Food',
         'price': 53000,
         'description': 'Premium cat food with essential nutrients',
-        'pet' :'Cat'},
+        'pet' :'Cat',
+        'image_url': 'https://www.petworld.ie/wp-content/uploads/2016/03/whiskas-meaty-meals-in-gravy-petworld.ie_.webp'},
 
-        {'product': 'Royal Canin Cat Food',
+
+        {'product': 'Whiskas Cat Food',
         'price': 36000,
         'description': 'Delicious cat food to keep your cat healthy.',
-        'pet' :'Cat'},
+        'pet' :'Cat',
+         'image_url': 'https://www.justdogsstore.com/wp-content/uploads/2021/07/61daAb0Hv2L._SL1000_.jpg'},
 
-        {'product': 'Me-O Kitten Food',
+        {'product': 'Whiskas Cat Milk',
         'price': 24000,
-        'description': 'Kitten food for your growing canine companion',
-        'pet' :'Cat'},
+        'description': 'Cat milk for your growing canine companion',
+        'pet' :'Cat',
+        'image_url': 'https://i0.wp.com/www.petmania.ie/wp-content/uploads/2021/11/54991.jpg?fit=600%2C600&ssl=1'},
 
-        {'product': 'Purina Dog Food',
+        {'product': 'Iams Dog Food',
         'price': 45000,
         'description': 'Nutritious dog food for active and playful dogs.',
-        'pet': 'Dog'},
+        'pet': 'Dog',
+         'image_url': 'https://www.iams.com/cdn-cgi/image/width=600,height=600,f=auto,quality=90/sites/g/files/fnmzdf3396/files/migrate-product-files/images/umdk8risrxj5a1vsuffs.png'},
 
-        {'product': 'Hills Science Diet Puppy Food',
+        {'product': 'Nature Science Diet Puppy Food',
         'price': 55000,
         'description': 'Specially formulated puppy food for optimal growth.',
-        'pet': 'Dog'},
+        'pet': 'Dog',
+         'image_url': 'https://cdn11.bigcommerce.com/s-lmdkge2gql/images/stencil/1280x1280/products/3790/4460/Nature%20Diet%20Grain%20Free%20Puppy__55984.1651786273.jpg?c=1'},
 
-        {'product': 'Pedigree Adult Dog Food',
+        {'product': 'Lucy Adult Dog Food',
         'price': 38000,
         'description': 'A balanced diet for adult dogs for overall health.',
-        'pet': 'Dog'},
+        'pet': 'Dog',
+         'image_url': 'https://www.lucypetproducts.com/wp-content/uploads/2019/07/ChBR_25lb-500x500.jpg.webp'}
     ]
 
     context = {
