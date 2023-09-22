fce6379651e255392afa7a1001e412b86c29550e
shafiroar <shafiranrm@gmail.com>
2023-09-22 14:57:07 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Item(models.Model):
     product = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     pet = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
